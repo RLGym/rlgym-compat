23f7f5d108ea02fd81330ec17234c82e2e637550
Jonathan Keegan <jpk3.14@gmail.com>
2023-06-20 12:29:19 -0400

    update game state and player to have additional RLGym data

--- rlgym_compat/game_state.py
+++ rlgym_compat/game_state.py
@@ -1,5 +1,5 @@
 import numpy as np
-from typing import List
+from typing import List, Optional
 
 from rlbot.utils.structures.game_data_struct import GameTickPacket, FieldInfoPacket, PlayerInfo
 
@@ -9,10 +9,15 @@ from .player_data import PlayerData
 
 class GameState:
     def __init__(self, game_info: FieldInfoPacket):
+        self.game_type: int = 0 # TODO: perhaps update this according to match settings
         self.blue_score = 0
         self.orange_score = 0
+        self.last_touch: Optional[int] = -1
+        
         self.players: List[PlayerData] = []
         self._on_ground_ticks = np.zeros(64)
+        self._air_time_since_jump = np.zeros(64)
+        self._has_flipped = np.zeros(64)
 
         self.ball: PhysicsObject = PhysicsObject()
         self.inverted_ball: PhysicsObject = PhysicsObject()
@@ -48,16 +53,28 @@ class GameState:
 
         if player_info.has_wheel_contact:
             self._on_ground_ticks[index] = 0
+            self._air_time_since_jump[index] = 0
         else:
             self._on_ground_ticks[index] += ticks_elapsed
+            if player_info.jumped: # Technically this should only start when you stop holding jump
+                self._air_time_since_jump[index] += ticks_elapsed
 
         player_data.car_id = index
         player_data.team_num = player_info.team
+        player_data.match_goals = player_info.score_info.goals
+        player_data.match_saves = player_info.score_info.saves
+        player_data.match_shots = player_info.score_info.shots
+        player_data.match_demolishes = player_info.score_info.demolitions
+        if player_data.boost_amount < player_info.boost / 100: # This isn't perfect but with decent fps it'll work
+            if player_data.boost_pickups == -1:
+                player_data.boost_pickups = 1
+            else:
+                player_data.boost_pickups += 1
         player_data.is_demoed = player_info.is_demolished
         player_data.on_ground = player_info.has_wheel_contact or self._on_ground_ticks[index] <= 6
         player_data.ball_touched = False
         player_data.has_jump = not player_info.jumped
-        player_data.has_flip = not player_info.double_jumped  # RLGym does consider the timer/unlimited flip, but i'm to lazy to track that in rlbot
+        player_data.has_flip = self._air_time_since_jump[index] < 150 and not player_info.double_jumped
         player_data.boost_amount = player_info.boost / 100
 
         return player_data
--- rlgym_compat/player_data.py
+++ rlgym_compat/player_data.py
@@ -5,6 +5,11 @@ class PlayerData(object):
     def __init__(self):
         self.car_id: int = -1
         self.team_num: int = -1
+        self.match_goals: int = -1
+        self.match_saves: int = -1
+        self.match_shots: int = -1
+        self.match_demolishes: int = -1
+        self.boost_pickups: int = -1
         self.is_demoed: bool = False
         self.on_ground: bool = False
         self.ball_touched: bool = False
